9970c56a3ef410b5786d91334b3952a185684aea
TolimanStaR <dima34511@gmail.com>
2020-04-02 01:08:23 +0300

    Standart login handler

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,13 @@
+# Using standart django authentication handlers:
+
+from django.contrib.auth import views as authentication_views
+
+# ...
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login_user, name='login'),
+    path('login/', authentication_views.LoginView.as_view(), name='login'),
+    path('logout/', authentication_views.LogoutView.as_view(), name='logout'),
 ]
