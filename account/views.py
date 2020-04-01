ea55e32747c57e49edb4f5a66557a776fb465888
TolimanStaR <dima34511@gmail.com>
2020-04-01 17:27:17 +0300

    auth login

--- account/views.py
+++ account/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 
-# Create your views here.
+from .forms import LoginForm
+
+
+def login_user(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+
+        user = None
+
+        if form.is_valid():
+            clean_data = form.cleaned_data
+            user = authenticate(
+                request,
+                username=clean_data['username'],
+                password=clean_data['password'],
+            )
+
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponse('Вы вошли в аккаунт')
+            else:
+                return HttpResponse('Аккаунт не активен')
+        else:
+            return HttpResponse('Пользователя с таким именем не существует!')
+    else:
+        form = LoginForm
+
+    return render(
+        request,
+        'account.login.html',
+        {'form': form}
+    )
