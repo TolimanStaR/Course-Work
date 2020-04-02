6bffba427acb9154b27b298198d0fe92c08ea6aa
TolimanStaR <dima34511@gmail.com>
2020-04-02 20:21:16 +0300

    user profile model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    register_date = models.DateTimeField(auto_now_add=True)
+    profile_image = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True, null=True)
+    about = models.CharField(max_length=400, blank=True)
+    contest_rating = models.PositiveIntegerField(default=0)
+    contest_rang = models.CharField(max_length=45, default='Начинающий')
+    contest_rang_color = models.PositiveIntegerField(default=0xAAAAAA)
+
+    def __str__(self):
+        return f'Профиль пользователя {self.user.username}'
