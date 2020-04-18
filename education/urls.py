4652f0b1cbf7627553bd814ecf35eb0b8aeece12
TolimanStaR <dima34511@gmail.com>
2020-04-18 23:08:25 +0300

    Course list and detail pages

--- education/urls.py
+++ education/urls.py
@@ -2,5 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.CourseListView.as_view(), name='course_list'),
+    path('',
+         views.CourseListView.as_view(),
+         name='course_list'),
+
+    path('subject/<slug:subject>/',
+         views.CourseListView.as_view(),
+         name='course_list_subject'),
+
+    path('course/<slug:slug>/',
+         views.CourseDetailView.as_view(),
+         name='course_detail'),
 ]
